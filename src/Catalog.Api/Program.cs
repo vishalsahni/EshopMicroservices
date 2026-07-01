fd00c40f07f98aa999e3e428561ad6ed8c199ee3
Sahni, Vishal <Vishal.Sahni+HON@Honeywell.com>
2026-07-01 06:26:18 +0530

    new changes

---
+++ src/Catalog.Api/Program.cs
@@ -0,0 +1,29 @@
+using Carter;
+using Marten;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+
+});
+
+builder.Services.AddCarter();
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapCarter();
+
+
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
